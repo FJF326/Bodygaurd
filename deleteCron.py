ed5b3be5fe91cc166c128429598b0ef0123c8999
Frank <ffiumef@clemson.edu>
2025-04-11 15:47:23 -0400

    deleteCron.py is working

--- deleteCron.py
+++ deleteCron.py
@@ -9,20 +9,25 @@ parser.add_argument("job", nargs='*', type=str)
 args = parser.parse_args()
 
 cron = open("/var/spool/cron/crontabs/"+args.user,"r")
-cronWrite = open("/var/spool/cron/crontabs/"+args.user, "w")
-
 info =cron.read()
 lines = info.splitlines()
 job = " ".join(args.job)
 count = 0
+
 print(job)
 for line in lines:
-    if job in line and line.startswith("#") == False:
+    if job in line:
+        print("found line")
         lines[count]=""
         break
     else:
-        coutn = count +1
+        count = count +1
 
+modified = ""
 for line in lines:
-    modified = modified + line
+    modified +=  line + "\n"
+
+print(modified)
+
+subprocess.run(['crontab', '-u',args.user,'-'], input=modified, text=True)
 
